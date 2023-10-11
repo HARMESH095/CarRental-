cc67ed2533336429f6829255cdf2ee574af23deb
HARMESH G V <116217472+HARMESH095@users.noreply.github.com>
2023-10-11 18:45:48 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         signUp sign = new signUp();
         System.out.println(" 1. Signup If you are new to this website");
-        System.out.println(" 2. ");
+        System.out.println(" 2. Login");
         sign.register();
         login.log();
 
